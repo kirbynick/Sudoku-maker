85739a86ca73305d69a5e873e3f6c67b438b4d3f
kirbynick <148408247+kirbynick@users.noreply.github.com>
2023-10-18 21:17:47 -0500

    Add files via upload

---
+++ Soduko Field/Program.cs
@@ -0,0 +1,48 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //int column = 1;
+        //int row = 1;
+
+        for (int row = 1; row<10; row++)
+        {
+
+            for (int column = 1; column<9; column++)
+            {
+                
+                Random random = new Random();
+
+                int num = random.Next(-9, 10);
+
+                if(num>0)
+                {
+                    Console.Write("[");
+                    Console.Write(num);
+                    Console.Write("]");  
+                } else
+                {
+                    Console.Write("[ ]");
+                }
+            }
+            
+                {
+                    Random random = new Random();
+
+                    int num1 = random.Next(-9, 10);
+
+                if(num1>0)
+                    {    
+                        Console.Write("[");
+                        Console.Write(num1);
+                        Console.WriteLine("]");
+                    } else
+                    {
+                        Console.WriteLine("[ ]");
+                    }
+                } 
+        }  
+    }
+
+        
+}
